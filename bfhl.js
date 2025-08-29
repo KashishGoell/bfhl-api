15304302662215e776e952972ca011307824ce1e
Kashish Goel <145182141+KashishGoell@users.noreply.github.com>
2025-08-29 11:00:28 +0530

    Add files via upload

---
+++ bfhl.js
@@ -0,0 +1,73 @@
+﻿const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.post('/bfhl', (req, res) => {
+  try {
+    const data = req.body.data || [];
+    
+    const full_name = "kashish";
+    const dob = "8222018045";
+    const email = "kashish2882005@gmail.com";
+    const roll_number = "22bce8559";
+    
+    const evenNumbers = [];
+    const oddNumbers = [];
+    const alphabets = [];
+    const specialChars = [];
+    let sum = 0;
+    let alphaChars = [];
+    
+    data.forEach(item => {
+      const str = String(item);
+      
+      if (!isNaN(str) && str.trim() !== '') {
+        const num = Number(str);
+        sum += num;
+        
+        if (num % 2 === 0) {
+          evenNumbers.push(str);
+        } else {
+          oddNumbers.push(str);
+        }
+      } else if (/^[a-zA-Z]+$/.test(str)) {
+        alphabets.push(str.toUpperCase());
+        alphaChars.unshift(...str.split(''));
+      } else {
+        specialChars.push(str);
+      }
+    });
+    
+    let concatString = "";
+    for (let i = 0; i < alphaChars.length; i++) {
+      if (i % 2 === 0) {
+        concatString += alphaChars[i].toUpperCase();
+      } else {
+        concatString += alphaChars[i].toLowerCase();
+      }
+    }
+    
+    const response = {
+      is_success: true,
+      user_id: \\_\\,
+      email: email,
+      roll_number: roll_number,
+      odd_numbers: oddNumbers,
+      even_numbers: evenNumbers,
+      alphabets: alphabets,
+      special_characters: specialChars,
+      sum: String(sum),
+      concat_string: concatString
+    };
+    
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({
+      is_success: false,
+      error: "An error occurred while processing the request"
+    });
+  }
+});
+
+module.exports = app;
